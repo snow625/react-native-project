3a4eb313854e8a33bd72c4d0c3ea12fde5fa7e8d
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2022-11-20 01:25:06 +0200

    deploy to expo

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider} from "react-redux";
-import {store} from "./redux/store"
+import {store} from "./src/redux/store"
 
 import { useFonts } from "expo-font";
 
-import Router from "./router";
+import Router from "./src/router";
 
 
 export default function App() {
 
   const [loaded] = useFonts({
-    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
-    RobotoMedium: require("./assets/fonts/Roboto-Medium.ttf"),
-    RobotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
+    RobotoRegular: require("./src/assets/fonts/Roboto-Regular.ttf"),
+    RobotoMedium: require("./src/assets/fonts/Roboto-Medium.ttf"),
+    RobotoBold: require("./src/assets/fonts/Roboto-Bold.ttf"),
   });
 
   if (!loaded) {
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
-1) ![alt text](https://i.ibb.co/j4Sqcn8/123.png)
-2) ![alt text](https://i.ibb.co/m8z5hsy/2.png)
+![alt text](./src//assets//qrCode.jpg)
+![alt text](https://i.ibb.co/m8z5hsy/2.png)
 ![alt text](https://i.ibb.co/1rN8TBw/2.png)
-3) ![alt text](https://i.ibb.co/VxWHqXg/3.png)
-4) ![alt text](https://i.ibb.co/fxSxyGb/4.png)
-5) ![alt text](https://i.ibb.co/wJf4YSc/5.png) 
+![alt text](https://i.ibb.co/VxWHqXg/3.png)
+![alt text](https://i.ibb.co/fxSxyGb/4.png)
+![alt text](https://i.ibb.co/wJf4YSc/5.png)
+
+
